05df537a3b1ab2de5f92dcfe7df8f98f993d01e1
johnkanel <gianniska99@gmail.com>
2022-11-09 12:25:23 +0200

    ASSIGNMENT_4

---
+++ module_4/script.js
@@ -0,0 +1,12 @@
+var names=["john","Joe","alex","nick","james"];
+
+for(var i=0;i<names.length;i++){
+	if (names[i][0]==="j" || names[i][0]==="J"){
+		console.log("Goodbye " +names[i]);
+	}
+    else 
+    {
+		console.log("hello " +names[i]);
+	}
+
+}
